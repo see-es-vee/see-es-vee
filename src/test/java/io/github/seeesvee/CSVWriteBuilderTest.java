7ad08fe32e5b865a76206e9f0793ac90a96fbae2
Gennaro Napolitano <gennaro@nau.edu>
2020-03-30 13:39:57 -0700

    added some tests, and pushing latest build

--- src/test/java/io/github/seeesvee/CSVWriteBuilderTest.java
+++ src/test/java/io/github/seeesvee/CSVWriteBuilderTest.java
@@ -1,4 +1,26 @@
 package io.github.seeesvee;
 
+import org.junit.jupiter.api.Test;
+
 public class CSVWriteBuilderTest {
+
+    @Test
+    void builderShouldSetClass() {
+
+    }
+
+    @Test
+    void builderShouldAddHandler() {
+
+    }
+
+    @Test
+    void builderShouldSetDelimiter() {
+
+    }
+
+    @Test
+    void builderShouldNotAllowNullClass()  {
+
+    }
 }
