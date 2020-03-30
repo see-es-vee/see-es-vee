ae8abf49a2da1eec6e442a0f2a83e7d2c6347c8c
Gennaro Napolitano <gennaro@nau.edu>
2020-03-30 15:23:56 -0700

    more unit test stuff?

--- src/test/java/io/github/seeesvee/CSVWriterTest.java
+++ src/test/java/io/github/seeesvee/CSVWriterTest.java
@@ -2,14 +2,15 @@ package io.github.seeesvee;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CSVWriterTest {
 
@@ -30,6 +31,17 @@ public class CSVWriterTest {
         testObject._float = 234.5f;
         testObject._int = 1337;
 
+        System.out.println("\n\n\nDelete writeFileTestOutput.csv\n\n\n");
+
+        ls();
+
+        Files.deleteIfExists(Paths.get("writeFileTestOutput.csv"));
+
+        System.out.println("\n\n\nShould be gone\n\n\n");
+
+
+        ls();
+
         ArrayList<TestClass> testArray = new ArrayList<>();
         testArray.add(testObject);
 
@@ -37,27 +49,19 @@ public class CSVWriterTest {
                 .setClass(TestClass.class)
                 .create();
 
-//        writer.write(new File("writeFileTestOutput.csv"), testArray);
-//
-//        MessageDigest md = MessageDigest.getInstance("SHA-256");
-//        String hex = checksum("writeFileTestOutput.csv", md);
+        writer.write(new File("writeFileTestOutput.csv"), testArray);
 
-        String s;
-        Process p;
-        try {
-            p = Runtime.getRuntime().exec("ls -aF");
-            BufferedReader br = new BufferedReader(
-                    new InputStreamReader(p.getInputStream()));
-            while ((s = br.readLine()) != null)
-                System.out.println("line: " + s);
-            p.waitFor();
-            System.out.println ("exit: " + p.exitValue());
-            p.destroy();
-        } catch (Exception e) {}
+        System.out.println("\n\n\n");
+
+        ls();
+
+        System.out.println("\n\n\n");
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        String hex = checksum("writeFileTestOutput.csv", md);
 
 
-
-//        assertEquals("3ea2f19bbdfbd73b29e55df257bbfa838563ff53fb1d4e09f5211520ad25505a", hex);
+        assertEquals("3ea2f19bbdfbd73b29e55df257bbfa838563ff53fb1d4e09f5211520ad25505a", hex);
 
     }
 
@@ -75,6 +79,22 @@ public class CSVWriterTest {
             result.append(String.format("%02x", b));
         }
         return result.toString();
+
+    }
+
+    private static void ls(){
+        String s;
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec("ls -aF");
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+            while ((s = br.readLine()) != null)
+                System.out.println("line: " + s);
+            p.waitFor();
+            System.out.println ("exit: " + p.exitValue());
+            p.destroy();
+        } catch (Exception e) {}
 
     }
 
